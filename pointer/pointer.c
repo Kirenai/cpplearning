2c3a68c753887ba351a8de8b5cb2142627be1e52
Kirenai <pool_dota2@hotmail.com>
2025-04-01 23:12:23 -0500

    punteros variables

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -8,4 +8,18 @@ int main() {
     for (int i = 0; i < 5; i++) {
         printf("value: %d\n", *(pointer + i));
     }
+
+    int number = 0;
+    int *pointer2 = &number;
+
+    printf("Value number: %d\n", number);
+    printf("Number address: %p\n", (void *) &number);
+
+    printf("Value pointer: %d\n", *pointer2);
+    printf("Address of pointer: %p\n", (void *) pointer2);
+
+    *pointer2 = 100;
+    printf("New value of number: %d\n", number);
+
+    return 0;
 }
