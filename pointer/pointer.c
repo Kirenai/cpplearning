2a735995a0a6283de8233c6781f288820ee1d562
Kirenai <pool_dota2@hotmail.com>
2025-04-01 22:38:40 -0500

    puntero y direccion de memoria

---
+++ pointer/pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+    int *pointer = NULL;
+
+    pointer = array;
+    for (int i = 0; i < 5; i++) {
+        printf("address value: %d\n", (pointer + i));
+    }
+}
